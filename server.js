b69e33d69cc25ba154f14d47413e3561f4dcef00
deepthi <danteswaridevi@gmail.com>
2026-06-18 12:40:11 +0530

    use mongodb atlas env variable

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 
-mongoose.connect("mongodb://127.0.0.1:27017/spiritualdb")
-.then(() => console.log("MongoDB Connected Successfully"))
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("MongoDB Atlas Connected"))
 .catch(err => console.log("MongoDB Error:", err));
 
 // ================= USER SCHEMA =================
